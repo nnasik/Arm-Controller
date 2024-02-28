45e615f3fb76aca96a49097285122226e47dcc2d
Nashath Nasik <mail2snasik@gmail.com>
2024-02-28 20:38:16 +0530

    Calibration feature update

--- Arm Controller/Form_Calibration.Designer.vb
+++ Arm Controller/Form_Calibration.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form_Calibration
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class Form_Calibration
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.TrackBar1 = New System.Windows.Forms.TrackBar()
         Me.TrackBar2 = New System.Windows.Forms.TrackBar()
@@ -46,6 +46,8 @@ Partial Class Form_Calibration
         Me.Label6 = New System.Windows.Forms.Label()
         Me.Label7 = New System.Windows.Forms.Label()
         Me.Label8 = New System.Windows.Forms.Label()
+        Me.TextBox_Serial = New System.Windows.Forms.TextBox()
+        Me.Button_Send = New System.Windows.Forms.Button()
         CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.TrackBar2, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.TrackBar3, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -57,7 +59,7 @@ Partial Class Form_Calibration
         '
         'TrackBar1
         '
-        Me.TrackBar1.Location = New System.Drawing.Point(179, 453)
+        Me.TrackBar1.Location = New System.Drawing.Point(221, 438)
         Me.TrackBar1.Maximum = 90
         Me.TrackBar1.Minimum = -90
         Me.TrackBar1.Name = "TrackBar1"
@@ -67,7 +69,7 @@ Partial Class Form_Calibration
         '
         'TrackBar2
         '
-        Me.TrackBar2.Location = New System.Drawing.Point(179, 391)
+        Me.TrackBar2.Location = New System.Drawing.Point(221, 376)
         Me.TrackBar2.Maximum = 90
         Me.TrackBar2.Minimum = -90
         Me.TrackBar2.Name = "TrackBar2"
@@ -77,7 +79,7 @@ Partial Class Form_Calibration
         '
         'TrackBar3
         '
-        Me.TrackBar3.Location = New System.Drawing.Point(179, 329)
+        Me.TrackBar3.Location = New System.Drawing.Point(221, 314)
         Me.TrackBar3.Maximum = 90
         Me.TrackBar3.Minimum = -90
         Me.TrackBar3.Name = "TrackBar3"
@@ -87,7 +89,7 @@ Partial Class Form_Calibration
         '
         'TrackBar4
         '
-        Me.TrackBar4.Location = New System.Drawing.Point(179, 267)
+        Me.TrackBar4.Location = New System.Drawing.Point(221, 252)
         Me.TrackBar4.Maximum = 90
         Me.TrackBar4.Minimum = -90
         Me.TrackBar4.Name = "TrackBar4"
@@ -97,7 +99,7 @@ Partial Class Form_Calibration
         '
         'TrackBar5
         '
-        Me.TrackBar5.Location = New System.Drawing.Point(179, 205)
+        Me.TrackBar5.Location = New System.Drawing.Point(221, 190)
         Me.TrackBar5.Maximum = 90
         Me.TrackBar5.Minimum = -90
         Me.TrackBar5.Name = "TrackBar5"
@@ -107,13 +109,14 @@ Partial Class Form_Calibration
         '
         'TrackBar6
         '
-        Me.TrackBar6.Location = New System.Drawing.Point(179, 143)
+        Me.TrackBar6.Location = New System.Drawing.Point(221, 128)
         Me.TrackBar6.Maximum = 90
         Me.TrackBar6.Minimum = -90
         Me.TrackBar6.Name = "TrackBar6"
         Me.TrackBar6.Size = New System.Drawing.Size(500, 56)
         Me.TrackBar6.TabIndex = 0
         Me.TrackBar6.TickFrequency = 5
+        Me.TrackBar6.Value = 90
         '
         'GroupBox1
         '
@@ -123,7 +126,7 @@ Partial Class Form_Calibration
         Me.GroupBox1.Controls.Add(Me.Label1)
         Me.GroupBox1.Controls.Add(Me.ComboBox_Serial)
         Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Location = New System.Drawing.Point(58, 12)
         Me.GroupBox1.Name = "GroupBox1"
         Me.GroupBox1.Size = New System.Drawing.Size(663, 98)
         Me.GroupBox1.TabIndex = 1
@@ -136,6 +139,7 @@ Partial Class Form_Calibration
         Me.Button_Connect.Name = "Button_Connect"
         Me.Button_Connect.Size = New System.Drawing.Size(134, 31)
         Me.Button_Connect.TabIndex = 3
+        Me.Button_Connect.Tag = "Connect"
         Me.Button_Connect.Text = "Connect"
         Me.Button_Connect.UseVisualStyleBackColor = True
         '
@@ -145,6 +149,7 @@ Partial Class Form_Calibration
         Me.ComboBox_Baudrate.Items.AddRange(New Object() {"300", "600", "750", "1200", "2400", "4800", "9600", "19200", "31250", "38400", "57600", "74880", "115200", "230400", "250000", "460800", "500000", "921600", "1000000", "2000000"})
         Me.ComboBox_Baudrate.Location = New System.Drawing.Point(346, 40)
         Me.ComboBox_Baudrate.Name = "ComboBox_Baudrate"
+        Me.ComboBox_Baudrate.RightToLeft = System.Windows.Forms.RightToLeft.No
         Me.ComboBox_Baudrate.Size = New System.Drawing.Size(156, 28)
         Me.ComboBox_Baudrate.TabIndex = 2
         '
@@ -169,25 +174,25 @@ Partial Class Form_Calibration
         'ComboBox_Serial
         '
         Me.ComboBox_Serial.FormattingEnabled = True
-        Me.ComboBox_Serial.Location = New System.Drawing.Point(113, 40)
+        Me.ComboBox_Serial.Location = New System.Drawing.Point(128, 40)
         Me.ComboBox_Serial.Name = "ComboBox_Serial"
-        Me.ComboBox_Serial.Size = New System.Drawing.Size(121, 28)
+        Me.ComboBox_Serial.Size = New System.Drawing.Size(106, 28)
         Me.ComboBox_Serial.TabIndex = 0
         '
         'Text_angle6
         '
         Me.Text_angle6.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Text_angle6.Location = New System.Drawing.Point(140, 143)
+        Me.Text_angle6.Location = New System.Drawing.Point(182, 128)
         Me.Text_angle6.Name = "Text_angle6"
         Me.Text_angle6.Size = New System.Drawing.Size(35, 27)
         Me.Text_angle6.TabIndex = 2
-        Me.Text_angle6.Text = "0"
+        Me.Text_angle6.Text = "90"
         Me.Text_angle6.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
         '
         'Text_angle5
         '
         Me.Text_angle5.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Text_angle5.Location = New System.Drawing.Point(140, 205)
+        Me.Text_angle5.Location = New System.Drawing.Point(182, 190)
         Me.Text_angle5.Name = "Text_angle5"
         Me.Text_angle5.Size = New System.Drawing.Size(35, 27)
         Me.Text_angle5.TabIndex = 2
@@ -197,7 +202,7 @@ Partial Class Form_Calibration
         'Text_angle4
         '
         Me.Text_angle4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Text_angle4.Location = New System.Drawing.Point(140, 267)
+        Me.Text_angle4.Location = New System.Drawing.Point(182, 252)
         Me.Text_angle4.Name = "Text_angle4"
         Me.Text_angle4.Size = New System.Drawing.Size(35, 27)
         Me.Text_angle4.TabIndex = 2
@@ -207,7 +212,7 @@ Partial Class Form_Calibration
         'Text_angle3
         '
         Me.Text_angle3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Text_angle3.Location = New System.Drawing.Point(140, 329)
+        Me.Text_angle3.Location = New System.Drawing.Point(182, 314)
         Me.Text_angle3.Name = "Text_angle3"
         Me.Text_angle3.Size = New System.Drawing.Size(35, 27)
         Me.Text_angle3.TabIndex = 2
@@ -217,7 +222,7 @@ Partial Class Form_Calibration
         'Text_angle2
         '
         Me.Text_angle2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Text_angle2.Location = New System.Drawing.Point(140, 390)
+        Me.Text_angle2.Location = New System.Drawing.Point(182, 375)
         Me.Text_angle2.Name = "Text_angle2"
         Me.Text_angle2.Size = New System.Drawing.Size(35, 27)
         Me.Text_angle2.TabIndex = 2
@@ -227,7 +232,7 @@ Partial Class Form_Calibration
         'Text_angle1
         '
         Me.Text_angle1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Text_angle1.Location = New System.Drawing.Point(140, 451)
+        Me.Text_angle1.Location = New System.Drawing.Point(182, 436)
         Me.Text_angle1.Name = "Text_angle1"
         Me.Text_angle1.Size = New System.Drawing.Size(35, 27)
         Me.Text_angle1.TabIndex = 2
@@ -238,7 +243,7 @@ Partial Class Form_Calibration
         '
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(12, 146)
+        Me.Label3.Location = New System.Drawing.Point(54, 131)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(65, 20)
         Me.Label3.TabIndex = 3
@@ -248,7 +253,7 @@ Partial Class Form_Calibration
         '
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(12, 208)
+        Me.Label4.Location = New System.Drawing.Point(54, 193)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(103, 20)
         Me.Label4.TabIndex = 3
@@ -258,7 +263,7 @@ Partial Class Form_Calibration
         '
         Me.Label5.AutoSize = True
         Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label5.Location = New System.Drawing.Point(12, 270)
+        Me.Label5.Location = New System.Drawing.Point(54, 255)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(49, 20)
         Me.Label5.TabIndex = 3
@@ -268,7 +273,7 @@ Partial Class Form_Calibration
         '
         Me.Label6.AutoSize = True
         Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label6.Location = New System.Drawing.Point(12, 332)
+        Me.Label6.Location = New System.Drawing.Point(54, 317)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(54, 20)
         Me.Label6.TabIndex = 3
@@ -278,7 +283,7 @@ Partial Class Form_Calibration
         '
         Me.Label7.AutoSize = True
         Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label7.Location = New System.Drawing.Point(12, 398)
+        Me.Label7.Location = New System.Drawing.Point(54, 383)
         Me.Label7.Name = "Label7"
         Me.Label7.Size = New System.Drawing.Size(75, 20)
         Me.Label7.TabIndex = 3
@@ -288,17 +293,39 @@ Partial Class Form_Calibration
         '
         Me.Label8.AutoSize = True
         Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label8.Location = New System.Drawing.Point(12, 456)
+        Me.Label8.Location = New System.Drawing.Point(54, 441)
         Me.Label8.Name = "Label8"
         Me.Label8.Size = New System.Drawing.Size(48, 20)
         Me.Label8.TabIndex = 3
         Me.Label8.Text = "Base"
         '
+        'TextBox_Serial
+        '
+        Me.TextBox_Serial.Font = New System.Drawing.Font("Courier New", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox_Serial.Location = New System.Drawing.Point(58, 540)
+        Me.TextBox_Serial.Multiline = True
+        Me.TextBox_Serial.Name = "TextBox_Serial"
+        Me.TextBox_Serial.ReadOnly = True
+        Me.TextBox_Serial.Size = New System.Drawing.Size(663, 151)
+        Me.TextBox_Serial.TabIndex = 4
+        '
+        'Button_Send
+        '
+        Me.Button_Send.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button_Send.Location = New System.Drawing.Point(633, 488)
+        Me.Button_Send.Name = "Button_Send"
+        Me.Button_Send.Size = New System.Drawing.Size(88, 36)
+        Me.Button_Send.TabIndex = 5
+        Me.Button_Send.Text = "Test"
+        Me.Button_Send.UseVisualStyleBackColor = True
+        '
         'Form_Calibration
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(682, 703)
+        Me.ClientSize = New System.Drawing.Size(782, 703)
+        Me.Controls.Add(Me.Button_Send)
+        Me.Controls.Add(Me.TextBox_Serial)
         Me.Controls.Add(Me.Label8)
         Me.Controls.Add(Me.Label7)
         Me.Controls.Add(Me.Label6)
@@ -357,4 +384,6 @@ Partial Class Form_Calibration
     Friend WithEvents Label7 As Label
     Friend WithEvents Label8 As Label
     Friend WithEvents Button_Connect As Button
+    Friend WithEvents TextBox_Serial As TextBox
+    Friend WithEvents Button_Send As Button
 End Class
--- Arm Controller/Form_Calibration.vb
+++ Arm Controller/Form_Calibration.vb
@@ -2,6 +2,8 @@
 Imports System.IO.Ports
 Imports System.Threading
 Imports System.Runtime.Serialization
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports System.ComponentModel
 
 Public Class Form_Calibration
     Dim angle_1, angle_2, angle_3, angle_4, angle_5, angle_6 As Integer
@@ -10,21 +12,25 @@ Public Class Form_Calibration
     Private Sub TrackBar6_Scroll(sender As Object, e As EventArgs) Handles TrackBar6.Scroll
         angle_6 = TrackBar6.Value
         Text_angle6.Text = angle_6
+
     End Sub
 
     Private Sub TrackBar4_Scroll(sender As Object, e As EventArgs) Handles TrackBar4.Scroll
         angle_4 = TrackBar4.Value
         Text_angle4.Text = angle_4
+
     End Sub
 
     Private Sub TrackBar3_Scroll(sender As Object, e As EventArgs) Handles TrackBar3.Scroll
         angle_3 = TrackBar3.Value
         Text_angle3.Text = angle_3
+
     End Sub
 
     Private Sub TrackBar2_Scroll(sender As Object, e As EventArgs) Handles TrackBar2.Scroll
         angle_2 = TrackBar2.Value
         Text_angle2.Text = angle_2
+
     End Sub
 
     Private Sub Text_angle6_TextChanged(sender As Object, e As EventArgs) Handles Text_angle6.TextChanged
@@ -52,13 +58,24 @@ Public Class Form_Calibration
     End Sub
 
     Private Sub Button_Connect_Click(sender As Object, e As EventArgs) Handles Button_Connect.Click
-        Try
-            ActiveSerialPort.BaudRate = Val(ComboBox_Baudrate.SelectedItem.ToString)
-            ActiveSerialPort.PortName = ComboBox_Serial.SelectedItem
-            ActiveSerialPort.Open()
-        Catch
-            ActiveSerialPort.Close()
-        End Try
+        If Button_Connect.Tag = "Connect" And Not ActiveSerialPort.IsOpen Then
+            OpenSerial()
+            If ActiveSerialPort.IsOpen Then
+                Button_Connect.Tag = "Disconnect"
+                Button_Connect.Text = "Disconnect"
+                ComboBox_Serial.Enabled = False
+                ComboBox_Baudrate.Enabled = False
+            End If
+        ElseIf Button_Connect.Tag = "Disconnect" And ActiveSerialPort.IsOpen Then
+            CloseSerial()
+            If Not ActiveSerialPort.IsOpen Then
+                Button_Connect.Tag = "Connect"
+                Button_Connect.Text = "Connect"
+                ComboBox_Serial.Enabled = True
+                ComboBox_Baudrate.Enabled = True
+            End If
+        End If
+
     End Sub
 
     Private Sub TrackBar5_Scroll(sender As Object, e As EventArgs) Handles TrackBar5.Scroll
@@ -69,6 +86,7 @@ Public Class Form_Calibration
     Private Sub TrackBar1_Scroll(sender As Object, e As EventArgs) Handles TrackBar1.Scroll
         angle_1 = TrackBar1.Value
         Text_angle1.Text = angle_1
+
     End Sub
 
     Private Sub Form_Calibration_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -76,23 +94,68 @@ Public Class Form_Calibration
         For Each Port In AvailablePorts
             ComboBox_Serial.Items.Add(Port.ToString)
         Next
-    End Sub
-
-    Private Sub ComboBox_Serial_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox_Serial.SelectedIndexChanged
         ComboBox_Baudrate.SelectedItem = "9600"
     End Sub
 
-    Private Sub SendSerial()
-        If ActiveSerialPort.IsOpen Then
 
+    Private Sub SendSerial(payload As String)
+        If ActiveSerialPort.IsOpen Then
+            Try
+                ActiveSerialPort.WriteLine(payload)       ' Write data to the send buffer
+            Catch ex As Exception           ' Exception handling            
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End If
+    End Sub
+
+    Private Sub Button_Send_Click(sender As Object, e As EventArgs) Handles Button_Send.Click
+        SendSerial(AngleString())
+    End Sub
+
+    Private Sub SerialPort1_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles ActiveSerialPort.DataReceived
+        ' Read the data from the serial port
+        Dim data As String = ActiveSerialPort.ReadLine()
+
+        ' Update the textbox with the received data
+        UpdateTextBox(data)
+    End Sub
+
+    Private Sub UpdateTextBox(data As String)
+        ' InvokeRequired compares the thread ID of the calling thread to the thread ID of the creating thread
+        ' If these threads are different, it returns true
+        If Me.TextBox_Serial.InvokeRequired Then
+            Dim d As New Action(Of String)(AddressOf UpdateTextBox)
+            Me.Invoke(d, New Object() {data})
+        Else
+
+            'Me.TextBox_Serial.AppendText(data & Environment.NewLine)
+            Me.TextBox_Serial.AppendText(data + vbNewLine)
         End If
     End Sub
 
     Private Sub OpenSerial()
-
+        Try
+            ActiveSerialPort.BaudRate = Val(ComboBox_Baudrate.SelectedItem.ToString)
+            ActiveSerialPort.PortName = ComboBox_Serial.SelectedItem
+            ActiveSerialPort.Open()
+        Catch
+            ActiveSerialPort.Close()
+        End Try
     End Sub
 
     Private Sub CloseSerial()
-
+        ActiveSerialPort.Close()
     End Sub
+
+    Private Sub Form_Calibration_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        CloseSerial()
+    End Sub
+
+
+    Function AngleString() As String
+        Return TrackBar1.Value.ToString + "," + TrackBar2.Value.ToString + "," + TrackBar3.Value.ToString + "," + TrackBar4.Value.ToString + "," + TrackBar5.Value.ToString + "," + TrackBar6.Value.ToString
+    End Function
+
+
+
 End Class
